14d6820c5421acbe2f1f87e8b11c20274e0523bc
Alex <alexundernazarov@gmail.com>
2022-10-18 15:08:16 +0300

    Задача_002

--- Задача_002/Program.cs
+++ Задача_002/Program.cs
@@ -10,7 +10,7 @@ a = 2, b = 10 -> нет
 a = 9, b =-3 -> да
 a = -3, b = 9 -> нет*/
 
-Console.WriteLine("Введите два числа: ");
+Console.Write("Введите два числа: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 == number2 * number2)
