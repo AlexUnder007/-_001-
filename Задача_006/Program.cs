d7b9aae4fa965cdc22250ccf26d682b72f8a142f
Alex <alexundernazarov@gmail.com>
2022-10-19 17:04:47 +0300

    Задача_006End

---
+++ Задача_006/Program.cs
@@ -0,0 +1,26 @@
+﻿/* 6. Напишите программу, которая 
+на вход принимает число и 
+выдаёт, является ли число чётным
+(делится ли оно на два без остатка).
+
+4 -> да
+-3 -> нет
+7 -> нет */
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 2 == 1)
+        {
+            Console.Write($"{number} Число нечётное ");
+        }
+        else
+        {
+            Console.Write($"{number} Число чётное ");
+        }
+    }
+}
--- задача_000/Program.cs
+++ задача_000/Program.cs
@@ -15,4 +15,3 @@ int number = Convert.ToInt32 (Console.ReadLine());
 
 int square = number * number;
 Console.WriteLine($"Квадрат числа: {number} = {square}");
-
