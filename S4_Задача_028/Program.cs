16e36451ff695cbd953f0eb9a14d85278660c273
Alex <alexundernazarov@gmail.com>
2022-11-15 18:05:08 +0300

    028

---
+++ S4_Задача_028/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая 
+// принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 ->120
+
+
+Console.WriteLine("Ввести число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Factorial(int number)
+{
+    int multiplay = 1; 
+    for (int i = 1; i <= number; i++)
+    {
+        //sum = sum + i; тоже самое
+        multiplay *= i;
+    }
+    return multiplay;
+}
+
+
+int factorial = Factorial(num);  //вызов метода
+Console.WriteLine($"Проихведение чисел от 1 до {num} = {factorial}");
