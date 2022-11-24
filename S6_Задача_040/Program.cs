813787495c7e2e9c2acd56903b4b7d15ea56dbd1
Alex <alexundernazarov@gmail.com>
2022-11-24 16:23:02 +0300

    040

---
+++ S6_Задача_040/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу: которая
+// принимает на вход три числа и 
+// проверяет: может ли существовать треугольник с сторонами такой длины]»
+
+// Каждая сторона треугольника меньше ссуммы двух сторон
+
+Console.WriteLine("Веедите три числа через Enter");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+bool IsExsistTriangle(int numberA, int numberB, int numberC)
+{
+    bool b = numberA < numberB + numberC && numberB < numberA + numberC && numberC < numberA + numberB; //Условие. Каждая сторона треугольника меньше ссуммы двух сторон
+    return b;
+}
+
+bool isExsistTriangle = IsExsistTriangle(num1,num2,num3);
+Console.WriteLine(isExsistTriangle);
